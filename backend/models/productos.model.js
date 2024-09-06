e031e115ca3e99befed939aa8776e8423df2a35a
sofia <cirosofia67@gmail.com>
2024-09-05 19:44:02 -0500

    crud productos

--- backend/models/productos.model.js
+++ backend/models/productos.model.js
@@ -2,10 +2,9 @@ const mongoose =  require('mongoose')
 const conexion = require("../config/database");
 
 const schemaProducto = new conexion.Schema({
-    /*referencia:{
+    referencia:{
         type: String,
-        required: [true, 'La referencia es Obligatoria'],
-    },*/
+        required: [true, 'La referencia es Obligatoria']},
     nombre: {
         type: String,
         required: [true, 'Asignar un nombre es obligatorio'],
